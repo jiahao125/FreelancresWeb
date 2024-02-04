56b6ef6290aa4dae39e82681d7b287d5cc4c2781
Kok Jia Hao <jjj_1994@hotmail.com>
2024-02-04 19:57:04 +0800

    Add project files.

---
+++ FreelancresWeb/Controllers/HomeController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System.Text;
+using Microsoft.CodeAnalysis;
+using System.Collections.ObjectModel;
+using FreelancresWeb.Models;
+using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+using System.Reflection.Metadata;
+
+namespace FreelancresWeb.Controllers
+{
+    public class HomeController:Controller
+    {
+        public List<Freelancers> freelancers1 { get; set; }
+        public async Task<IActionResult> Index()
+        {
+            await GetUser();
+            return View();
+        }
+        private async Task GetUser()
+        {
+            var client = new HttpClient();
+            //var content = new StringContent(json, Encoding.UTF8, "application/json");
+            HttpResponseMessage response = await client.GetAsync("https://localhost:7138/GetFreelances");
+            if (response != null)
+            {
+                var jsonString = await response.Content.ReadAsStringAsync();
+                freelancers1 = JsonConvert.DeserializeObject<List<Freelancers>>(jsonString);
+            }
+            ViewBag.User = freelancers1;
+        }
+
+        public async Task<IActionResult> Insert_PostBack(string userName)
+        {
+            var client = new HttpClient();
+            string json = JsonConvert.SerializeObject(userName);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            HttpResponseMessage response = await client.PostAsync("https://localhost:7138/AddFreelancers?userName="+userName, content);
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+                
+        }
+
+        public async Task<IActionResult> Update_PostBack(string oldName,string newName)
+        {
+            var client = new HttpClient();
+            string json = JsonConvert.SerializeObject(oldName + newName);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            HttpResponseMessage response = await client.PutAsync("https://localhost:7138/UpdateFreelancer?oldName=" + oldName +"&newName=" +newName, content);
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+
+        }
+
+        public async Task<IActionResult> Delete_PostBack(string userName)
+        {
+            var client = new HttpClient();
+            string json = JsonConvert.SerializeObject(userName);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            HttpResponseMessage response = await client.DeleteAsync("https://localhost:7138/DeleteFreelancers?userName=" + userName);
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+
+        }
+
+    }
+}
---
+++ FreelancresWeb/Models/Freelancers.cs
@@ -0,0 +1,12 @@
+﻿namespace FreelancresWeb.Models
+{
+    public class Freelancers
+    {
+        public Guid RowId { get; set; }
+        public string? UserName { get; set; }
+        public string? EmailAddress { get; set; }
+        public string? PhoneNum { get; set; }
+        public string? SkillSet { get; set; }
+        public string? Hobby { get; set; }
+    }
+}
